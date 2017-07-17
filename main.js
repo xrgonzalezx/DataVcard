536b39970ff07f0bee81ee37cf5b711ddc0f48b8
Rudy Gonzalez <xrgonzalezx@gmail.com>
2017-07-17 18:40:45 -0400

    secondversion

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 1. First, find our UL Container
 let basics = document.querySelector(".basics")
 let story = document.querySelector(".story")
-
+let picture = document.querySelector(".picture")
 
 
 // 2. Create our Ajax Request
@@ -11,36 +11,23 @@ req.addEventListener("load", function(){
 let object = JSON.parse(req.response)
   console.log(object.results);
   basics.innerHTML += `
-  <p> Name: ${object.name}<p><br>
-  <p> Github URL: <a href ="${object.html_url}">xrgonzalezx</a></p>
-  <p> Github #: ${object.id}<p><br>
-  <p> Company: ${object.company}<p><br>
-  <p> Website: ${object.blog}<p><br>
-  `});
+  <h2>The Basics</h2>
+  <ul>
+    <li><span> Name: </span> ${object.name}</li>
+    <li><span> Github URL: </span> </h4> <a href ="${object.html_url}">xrgonzalezx</a></li>
+    <li><span> Github #: </span> ${object.id}</li>
+    <li><span> Company: </span> ${object.company}</li>
+    <li><span> Website: </span> <a href="${object.blog}">www.rudyg.com</a></li>
+  </ul>
+`
+
+story.innerHTML += `
+<h2>The Story</h2>
+<p> ${object.bio}<p>
+`
+picture.innerHTML +=`
+<img src="${object.avatar_url}">
+`
+
+});
 req.send();
-
-// let req = new XMLHttpRequest();
-// req.open("GET", "https://api.github.com/users/xrgonzalezx");
-// req.addEventListener("load", function(){
-// let object = JSON.parse(req.response)
-//   console.log(object.results);
-//   basics.innerHTML += `
-//   <p>${object.bio}<p><br>
-//   `});
-// req.send();
-
-
-
-
-
-
-
-
-// fetch("https://github.com/xrgonzalezx").then(function (data) {
-//   return data.json()
-// })
-// .then(function (obj) {
-//   obj.results.forEach(function(character) {
-//     ul.innerHTML += `<li>${character.name}</li>`
-//   })
-// })
